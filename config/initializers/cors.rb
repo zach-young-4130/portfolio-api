e9a750240e943b8c7d94fbde7c1548e97a5f67c4
Zach Young <zachwillwork@gmail.com>
2026-06-09 23:45:55 -0500

    Update allowed origins within cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:4200", "http://127.0.0.1:4200",
-            "https://zachyoung.com", "https://www.zachyoung.com",
             "https://zachyoung.org", "https://www.zachyoung.org",
             /https:\/\/.*\.vercel\.app/
 
